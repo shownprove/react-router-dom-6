db0f5b3214b4e509b3de49de20a3af9051a65ffa
oddcode <oddcode@naver.com>
2022-12-25 21:39:20 +0900

    useNavigate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const onHomeClick = () => {
+    navigate("/");
+  };
+  const onAboutClick = () => {
+    navigate("/about");
+  };
   return (
     <header>
       <ul>
         <li>
-          <Link to={"/"}>Home</Link>
+          <button onClick={onHomeClick}>Home</button>
         </li>
         <li>
-          <Link to={"/about"}>About</Link>
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul>
     </header>
